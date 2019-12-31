ef9261d72d2db0dc28e89b6d349c58bc83b12178
Alain Christopher <alain.brusch@me.com>
2019-12-31 17:12:54 +0100

    filter

--- assets/Javascript/filter.js
+++ assets/Javascript/filter.js
@@ -2,26 +2,71 @@ $(document).ready(function(){
 $('#filters').hide() 
 })
 
-$('#activeFilter').on('click',function(){
-    $('#filters').show() 
+$('.show-filters').on('click',function(){
+    $('#filters').toggle() 
+    $('header').toggleClass('expanded')
     })
 
+
 $('.Corporate').on('click', function(){
-  
-$('.filterSection').addClass('hidden')
 
 $('section.Corporate').show() 
 $('section.Grafik').hide() 
 $('section.Publik').hide() 
 $('section.Fotografie').hide() 
 $('section.Digital').hide()
-
-$('.filterSection').removeClass('hidden')
-
+$('#filters').hide() 
+$('header').toggleClass('expanded')
 })
 
+$('.Grafik').on('click', function(){
 
-$('.all').on('click', function(){
+    $('section.Corporate').hide() 
+    $('section.Grafik').show() 
+    $('section.Publik').hide() 
+    $('section.Fotografie').hide() 
+    $('section.Digital').hide()
+    $('#filters').hide() 
+    $('header').toggleClass('expanded')
+    })
+
+    $('.Publik').on('click', function(){
+
+        $('section.Corporate').hide() 
+        $('section.Grafik').hide() 
+        $('section.Publik').show() 
+        $('section.Fotografie').hide() 
+        $('section.Digital').hide()
+        $('#filters').hide() 
+        $('header').toggleClass('expanded')
+        })
+
+        $('.Fotografie').on('click', function(){
+
+            $('section.Corporate').hide() 
+            $('section.Grafik').hide() 
+            $('section.Publik').hide() 
+            $('section.Fotografie').show() 
+            $('section.Digital').hide()
+            $('#filters').hide() 
+            $('header').toggleClass('expanded')
+            })
+    
+            $('.Digital').on('click', function(){
+
+                $('section.Corporate').hide() 
+                $('section.Grafik').hide() 
+                $('section.Publik').hide() 
+                $('section.Fotografie').hide() 
+                $('section.Digital').show()
+                $('#filters').hide() 
+                $('header').toggleClass('expanded')
+                })
+
+
+
+
+$('.show-filters').on('click', function(){
 
 $('section').show() 
 
